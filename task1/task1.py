7b6e0a0ec4ad73af4f52df7e5dbfff7252dfaf4e
MaryKreona <129825953+MaryKreona@users.noreply.github.com>
2025-03-18 22:39:44 +0700

    18

--- task1/task1.py
+++ task1/task1.py
@@ -1,23 +1,15 @@
 def find_path(n, m):
-    # Создаем массив от 1 до n
-    array = list(range(1, n + 1))
-    # Создаем пустой список
+    arr = list(range(1, n + 1))
     path = []
-    # Начинаем с первого элемента
     current = 0
 
     while True:
-        # Добавляем текущий элемент в путь
-        path.append(array[current])
-        # Вычисляем следующий элемент, двигаясь на m шагов
+        path.append(arr[current])
         current = (current + m - 1) % n
-        # Если вернулись к первому элементу, завершаем цикл
         if current == 0:
             break
-
     return path
 
 
-# Пример использования
 n, m = map(int, input().split())
 print(*find_path(n, m))
